7e56a1aa88a25d74459b99abe6f4947c45c683c2
gimenodaryll@gmail.com <gimenodaryll@gmail.com>
2025-09-12 01:11:41 +0800

    Add project files.

---
+++ Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Fashioninventorysystem.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
